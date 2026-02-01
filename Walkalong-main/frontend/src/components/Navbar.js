714f78e40a8b39408d29b5fa43552ea92e39dfa5
kavyaasuresh <kavyaasuresh11@gmail.com>
2026-02-01 23:46:34 +0530

    resolved issue

--- Walkalong-main/frontend/src/components/Navbar.js
+++ Walkalong-main/frontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Home, BookOpen, CheckSquare, Layers, ListTodo, Calendar, Heart, Compass } from 'lucide-react';
+import { Home, BookOpen, CheckSquare, Layers, ListTodo, Calendar, Heart, Compass, Sun, Moon } from 'lucide-react';
 import './Navbar.css';
 
 const Navbar = () => {
@@ -17,45 +17,6 @@ const Navbar = () => {
         <div className="navbar-menu">
           <Link
             to="/dashboard"
-            className={`navbar-item ${location.pathname === '/dashboard' || location.pathname === '/' ? 'active' : ''}`}
-          >
-            <Home size={18} />
-            <span>Dashboard</span>
-          </Link>
-
-          <Link
-            to="/streams"
-            className={`navbar-item ${location.pathname === '/streams' ? 'active' : ''}`}
-          >
-            <Layers size={18} />
-            <span>Streams</span>
-          </Link>
-
-          <Link
-            to="/tasks"
-            className={`navbar-item ${location.pathname === '/tasks' ? 'active' : ''}`}
-          >
-            <ListTodo size={18} />
-            <span>Tasks</span>
-          </Link>
-
-          <Link
-            to="/viewplan"
-            className={`navbar-item ${location.pathname === '/viewplan' ? 'active' : ''}`}
-          >
-            <Calendar size={18} />
-            <span>View Plan</span>
-          </Link>
-
-          <Link
-            to="/mood"
-            className={`navbar-item ${location.pathname === '/mood' ? 'active' : ''}`}
-          >
-            <Heart size={18} />
-            <span>Mood</span>
-          </Link>
-
-          <Link
             to="/workdone"
             className={`navbar-item ${location.pathname === '/workdone' ? 'active' : ''}`}
           >
